a1964b23157737c2f08b98b46baa88087e3650d2
Czerniawski <czerniah@emea.roche.com>
2022-06-27 19:45:51 +0200

    Added abilty to count correct answers and disabled buttons after clicking

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -11,23 +12,97 @@ import com.google.android.material.snackbar.Snackbar
 //import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.example.geoquiz.databinding.ActivityMainBinding
 
-private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     private val questionBank = listOf(
-        Question(R.string.question_africa, false),
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true),
+        Question(R.string.question_africa, false, false),
+        Question(R.string.question_australia, true,false),
+        Question(R.string.question_oceans, true, false),
+        Question(R.string.question_mideast, false, false),
+        Question(R.string.question_americas, true, false),
+        Question(R.string.question_asia, true, false),
     )
 
+    private val TAG = "MainActivity"
     private var currentIndex = 0
+    private val KEY_INDEX = "index"
+    // Challenge - Counters to keep track of number of questions answered and how many correct
+    private var answeredQuestions = 0
+    private var correctAnswers = 0
+    // Array to save the state of which questions were answered the size of mQuestion Bank
+    private var whichAnswered = BooleanArray(questionBank.size)
 
+    // KEYS to save state if user rotates device and the Activity is recreated
+    private val KEY_ANSWERED_QUESTIONS = "number_answered"
+    private val KEY_CORRECT_ANSWERS = "number_correct"
+    private val KEY_WHICH_QUESTIONS_ANSWERED = "which_answered"
+
+    private fun toast(messageResID: Int) {
+        Toast.makeText(this, messageResID, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun checkAnswer(userPressedTrue: Boolean) {
+        val answerIstrue = questionBank[currentIndex].answer
+
+        // Challenge - Mark question as answered and disable buttons and count answered questions
+        questionBank[currentIndex].alreadyAnswered = true
+        buttonsEnabled(false)
+        answeredQuestions++
+
+        var messageResId = 0
+
+        if (userPressedTrue == answerIstrue) {
+            messageResId = R.string.correct_toast
+            correctAnswers++
+        } else {
+            messageResId = R.string.incorrect_toast
+        }
+
+        toast(messageResId)
+        calculateScore()
+    }
+
+    private fun updateQuestion() {
+        val question = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(question)
+
+        // Challenge - disable or enable answer buttons depending if the Question was already answered
+        if (!questionBank[currentIndex].alreadyAnswered) {
+            buttonsEnabled(true)
+        } else buttonsEnabled(false)
+    }
+
+    // Challenge utility method for enabling or disabling buttons
+    private fun buttonsEnabled(enabled: Boolean) {
+        binding.trueButton.isEnabled = enabled
+        binding.falseButton.isEnabled = enabled
+    }
+
+    // Challenge method to calculate score
+    private fun calculateScore() {
+        val totalQuestions = questionBank.size
+        val score = correctAnswers * 100 / totalQuestions
+
+        // Challenge - Only shows score if we answered all the questions, and then rests all
+        if (answeredQuestions == totalQuestions) {
+            val message = "You scored $score% correct answers. Resetting all"
+            val toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+            // reset
+            correctAnswers = 0
+            answeredQuestions = 0
+            for (question in questionBank) {
+                question.alreadyAnswered = false
+            }
+
+        }
+    }
+
+
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -88,23 +163,8 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onDestroy() called")
     }
 
-    private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        binding.questionTextView.setText(questionTextResId)
 
-    }
 
-    private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
-        }
-
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
-    }
 
 }
 
